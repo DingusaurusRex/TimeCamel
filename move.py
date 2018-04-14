b47ba17078fd1c9ba88484b048faacb44d4c8076
Jack <jfansea@gmail.com>
2018-04-14 13:02:23 -0700

    Creating a pojo for a move
    Wrapper for all data needed to generate the move array to return

---
+++ move.py
@@ -0,0 +1,44 @@
+# Move Constants
+ROLL_MOVE = 0
+PLACE_TRAP_MOVE = 1
+PLACE_ROUND_BET = 2
+PLACE_GAME_WINNER_BET = 3
+PLACE_GAME_LOSER_BET = 4
+
+# Trap Constants
+MIRAGE = -1
+OASIS = 1
+
+moveType = 0
+trapType = 0
+trapLocation = 0
+chosenCamel = 0
+
+def __init__(self, moveType):
+	self.moveType = moveType
+	
+def setTrapType(trapType):
+	self.trapType = trapType
+	return self
+	
+def setTrapLocation(trapLocation):
+	self.trapLocation = trapLocation
+	return self
+
+def setChosenCamel(chosenCamel):
+	self.chosenCamel = chosenCamel
+	return self
+	
+def generateMoveArray():
+	if moveType = ROLL_MOVE:
+		return [ROLL_MOVE]
+	elif moveType = PLACE_TRAP_MOVE:
+		return [PLACE_TRAP_MOVE, trapType, trapLocation]
+	elif: moveType = PLACE_ROUND_BET:
+		return [PLACE_ROUND_BET, chosenCamel]
+	elif: moveType = PLACE_GAME_WINNER_BET:
+		return [PLACE_GAME_WINNER_BET, chosenCamel]
+	elif: moveType = PLACE_GAME_LOSER_BET:
+		return [PLACE_GAME_LOSER_BET, chosenCamel]
+	else:
+		return none
